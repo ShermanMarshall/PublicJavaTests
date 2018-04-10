e01e21f08f1e858f0d479e404abc2578b12cc44e
Sherman Marshall <sherman.david.marshall@gmail.com>
2018-04-09 23:11:35 -0700

    Update to test writing, NIO vs BufferedWriting

--- bestpractices/io/NIOForFiles.java
+++ bestpractices/io/NIOForFiles.java
@@ -20,8 +20,11 @@ import java.util.ArrayList;
 /**
  * @class:       NIOForFiles
  * @description: Performs a simple test to compare the efficiency of NIO against the older Buffered IO implementation.
-		 NIO outperforms, by a significant margin, per my tests. These results may be effected further by the 
-		 size of the allocated ByteBuffer. I will likely revisit this in the future to test writing as well
+ *               My tests suggest NIO outperforms BufferedReading by a significant margin; however, BufferedWriting 
+ *               appears to have the advantage over NIO. Somewhat odd results; however, the below code should consistently
+ *               suggest the same. These results may be effected by the size of the allocated ByteBuffer. OOM errors occurring
+ *               from large ByteBuffers required reduced size relative to the BufferedWriter--though, that may be more reason
+ *               to favor the old way.
  * @author:      Sherman
  */
 public class NIOForFiles {
@@ -44,7 +47,7 @@ public class NIOForFiles {
 		if ((num % (int) (Math.pow(2, 27))) == 0) {
 		    bw.write(output.toString());
 		    bw.flush();
-		    output = new StringBuilder();
+		    output.delete(0, output.length());
 		}   
 	    }   
 
@@ -52,6 +55,36 @@ public class NIOForFiles {
 	} catch (IOException ioe) {
 		System.out.println(ioe);
 	}   
+    }
+
+    public static void createGBFileWithNIO() {
+	try (FileChannel channel = (FileChannel) Files.newByteChannel( Paths.get("oneGbOfCapsNIO.txt"),
+						StandardOpenOption.WRITE, StandardOpenOption.CREATE ) ) {
+		ByteBuffer buffer = ByteBuffer.allocate((int) Math.pow(2, 26));
+		int max = (int) Math.pow(2, 30);
+		int total = 0, val = 0;
+		StringBuilder output = new StringBuilder();
+		while (total < max) {
+			total++;
+			val = (int) (Math.random() * 28);
+			if (val == 0 || val == 27) {
+				output.append("\n");
+			} else {
+				output.append((char) (val + 64));
+			}
+			if ((total % (int) (Math.pow(2, 26))) == 0) {
+				buffer.put(output.toString().getBytes());
+				buffer.flip();
+				channel.write(buffer);
+				buffer.clear();
+				output.delete(0, output.length());
+			}
+		}
+	} catch (InvalidPathException e) {
+		System.out.println(e);
+	} catch (IOException ioe) {
+		System.out.println(ioe);
+	}
     }
     
     public static String readFile(String filename) {
@@ -63,15 +96,14 @@ public class NIOForFiles {
             int bytesRead = 0;
             
             while ((bytesRead = inputChannel.read(buf)) > -1) {
+		buf.rewind();
                 if (bytesRead < buf.limit()) {
-                    buf.rewind();
                     byte[] remaining = new byte[bytesRead];
                     buf.get(remaining);
 		    new String(remaining);
                 } else {
 		    new String(buf.array());
                 }
-                buf.rewind();
             }
         } catch (IOException ioe) {
             System.out.println(ioe);
@@ -82,43 +114,13 @@ public class NIOForFiles {
         return output;
     }
     
-    public static boolean writeFile(String content, String filepath) {
-        Path path = null;
-        
-        try {
-            path = Paths.get(filepath);
-        } catch (InvalidPathException ipe) {
-            System.out.println(ipe);
-            return false;
-        }
-        
-        try (FileChannel fileChannel = (FileChannel) Files.newByteChannel(
-                                                        Paths.get(filepath), 
-                                                        StandardOpenOption.WRITE, 
-                                                        StandardOpenOption.CREATE)) {
-            
-            ByteBuffer buffer = ByteBuffer.allocate(content.length());
-            buffer.put(content.getBytes());
-            buffer.rewind();
-            
-            fileChannel.write(buffer);
-            
-        } catch (IOException ioe) {
-            System.out.println(ioe);
-        } catch (InvalidPathException ipe) {
-            System.out.println(ipe);
-        }
-        
-        return true;
-    }
-    
     public static String bufferedRead(String filename) {
         
         try (BufferedReader br = new BufferedReader(new FileReader(new File(filename)))) {
             String input;
             while ((input = br.readLine()) != null) {
-		//Empty string to save memory. JVM will collect it at some point
-                new String(input.getBytes());
+		//Empty string to simulate activity. JVM will collect it at some point
+                //new String(input.getBytes());
             }
         } catch (IOException ioe) {
             System.out.println(ioe);
@@ -127,26 +129,16 @@ public class NIOForFiles {
         return "";
     }
 
-    public static void bufferedWrite(String filename) {
-
-	try (BufferedWriter bw = new BufferedWriter(new FileWriter(new File(filename)))) {
-
-	} catch (IOException ioe) {
-
-	}
-    }
-    
-    public static void main(String...args) {
+    public static void testReading(String...args) {
 	String filename = "oneGbOfCaps.txt";
 
 	if (!new File(filename).isFile()) {
 	    createGBFile();
+		return;
  	}
 
         ArrayList<Double> nioReadMetrics = new ArrayList();
 	ArrayList<Double> bufferedReadMetrics = new ArrayList();
-	//ArrayList<Double> nioWriteMetrics = new ArrayList();
-	//ArrayList<Double> bufferedWriteMetrics = new ArrayList();
 
 	double difference = 0.0;
         for (int x = 0; x < 10; x++) {
@@ -183,27 +175,51 @@ public class NIOForFiles {
 
 	average /= bufferedReadMetrics.size();
 	System.out.println("Average for buffered reading: " + average);
+    }
 
-	/*
+    public static void testWriting(String...args) {
+	ArrayList<Double> nioWriteMetrics = new ArrayList();
+	ArrayList<Double> bufferedWriteMetrics = new ArrayList();
+
+	double difference = 0.0;
 	for (int x = 0; x < 10; x++) {
-            System.out.println("Reading file with NIO");
+	    System.out.println("Writing file with NIO");
 
-            long time = System.currentTimeMillis();
-            String output = readFile(filename);
+	    long time = System.currentTimeMillis();
+	    createGBFileWithNIO();
 
-            difference = (((double)System.currentTimeMillis() - time) / 1000.0);
-            System.out.println("time: " + difference);
-            nioReadMetrics.add(difference);
-
-            System.out.println("Reading file with Buffered Reader");
-            time = System.currentTimeMillis();
-
-            bufferedRead(filename);
-            difference = (((double)System.currentTimeMillis() - time) / 1000.0);
+	    difference = (((double)System.currentTimeMillis() - time) / 1000.0);
 	    System.out.println("time: " + difference);
-            bufferedReadMetrics.add(difference);
-        }
-	*/
+	    nioWriteMetrics.add(difference);
+
+	    System.out.println("Writing file with Buffered Writer");
+	    time = System.currentTimeMillis();
+
+	    createGBFile();
+	    difference = (((double)System.currentTimeMillis() - time) / 1000.0);
+	    System.out.println("time: " + difference);
+	    bufferedWriteMetrics.add(difference);
+	}
+
+	double average = 0.0;
+	for (Double d : nioWriteMetrics) {
+	    average += d;
+	}
+	average /= nioWriteMetrics.size();
+
+	System.out.println("Average for NIO writing: " + average);
+
+	average = 0;
+	for (Double d : bufferedWriteMetrics) {
+	    average += d;
+	}
+
+	average /= bufferedWriteMetrics.size();
+	System.out.println("Average for buffered writing: " + average);
+    }	
+
+    public static void main (String...args) {
+	//testReading();
+	testWriting();
     }
 }
-
