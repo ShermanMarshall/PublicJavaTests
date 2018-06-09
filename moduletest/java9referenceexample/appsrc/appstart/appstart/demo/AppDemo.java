b94d1189067d73ad1223b2daa761136d559343bd
Sherman Marshall <shermanmarshall@Shermans-Mac-mini.local>
2018-06-09 14:36:18 -0700

    Basic module example from the reference

---
+++ moduletest/java9referenceexample/appsrc/appstart/appstart/demo/AppDemo.java
@@ -0,0 +1,9 @@
+package appstart.demo;
+
+import appfuncs.demo.Test;
+
+public class AppDemo {
+        public static void main(String...args) {
+                        System.out.println("2 is a factor of 10: " + Test.isFactor(2, 10));
+        }   
+}
