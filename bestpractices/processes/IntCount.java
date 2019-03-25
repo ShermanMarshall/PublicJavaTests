03366ecab5714e8b29a5882295c299b5f23d526a
Sherman Marshall <sherman.david.marshall@gmail.com>
2019-03-24 22:58:27 -0700

    Test works! Finally get expected output

---
+++ bestpractices/processes/IntCount.java
@@ -0,0 +1,58 @@
+package processes;
+
+import java.io.*;
+
+public class IntCount {
+	public static void main(String...args) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		int x = 0;
+
+		ProcessBuilder pb = new ProcessBuilder(new String[]{ "java", "processes.ReadInt" });
+		pb.redirectInput(ProcessBuilder.Redirect.PIPE);
+		pb.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+
+		Process p = pb.start();
+
+		OutputStream os = p.getOutputStream();
+		InputStream is = p.getInputStream();
+		
+		byte[] data = new byte[4096];
+		int bytesRead = 0;
+		
+		StringBuilder output = new StringBuilder();
+		boolean isBlockingForInput = true;
+
+		do {
+			sb.append(x++);
+			if (x % 1024 == 0) {
+				os.write(sb.toString().getBytes());
+				os.write("\0\3\4".getBytes());
+				os.flush();
+				
+				sb.delete(0, sb.length());
+				try {
+					Thread.sleep(50);
+				} catch (InterruptedException ie) {
+					System.out.println(ie);
+				}
+
+				while (isBlockingForInput && ((bytesRead = is.read(data)) != -1)) {
+					if (bytesRead == data.length) {
+						output.append(new String(data));
+					} else {
+						byte[] subset = new byte[bytesRead];
+						System.arraycopy(data, 0, subset, 0, bytesRead);
+						output.append(new String(subset));
+					}
+				}
+				
+				System.out.println(output.toString());
+				output.delete(0, output.length());
+				System.out.println("input complete");
+			}
+		} while(x < (1024 * 1024));
+
+		os.write("exit".getBytes());
+		os.close();
+	}
+}
---
+++ bestpractices/processes/ReadInt.java
@@ -0,0 +1,48 @@
+package processes;
+
+import java.io.*;
+
+public class ReadInt { 
+	public static boolean eot (String str) {
+		return str.charAt(str.length() - 3) == '\0' &&
+			str.charAt(str.length() - 2) == '\3' &&
+			str.charAt(str.length() - 1) == '\4';
+	}
+
+	public static void main(String...args) {
+		InputStream is = System.in;
+
+		byte[] data = new byte[4096];
+		int bytesRead = 0;
+
+		String content = "";
+		
+		try (PrintWriter pw = new PrintWriter(new FileWriter(new File("write_ints.txt")))) {
+			StringBuilder sb = new StringBuilder();
+			int count = 1;
+
+			while (!content.contains("exit")) {
+				while (((bytesRead = is.read(data)) != -1)) {
+					if (bytesRead == data.length) {
+						content = new String(data);
+					} else {
+						byte[] subset = new byte[bytesRead];
+						System.arraycopy(data, 0, subset, 0, bytesRead);
+						content = new String(subset);
+					}
+					sb.append(content.substring(0, content.length() - 3));
+					if (eot(content)) {
+						pw.println(sb.toString());
+						sb.delete(0, sb.length());
+						pw.flush();
+						System.out.write(new StringBuilder("Wrote to file: " + count++ + " times").toString().getBytes());
+					}
+				}
+			}
+			
+			pw.close();
+		} catch (IOException ioe) {
+			System.out.println(ioe);
+		}
+	}
+}
